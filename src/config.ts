7115f38b766931e06f32fe6c610c6f5bc7debe01
free-guru <emanuelaraujopadilha@gmail.com>
2024-03-22 22:30:35 -0300

    Update config file

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJQb2x5UHVwIiwiaWF0IjoxNzEwNzk5MjU0LCJuYW1lIjoiSHlwZXJzcGFjZSBBVkFYIn0.1SEiyIL8JhuTVMypmhdpnKUJYuqvj9bWpP8EjMt_AW4";
+  "Your api key";
 export const RPC_URL = "https://api.avax.network/ext/bc/C/rpc";
 export const PRIVATE_KEY = "Your wallet private key";
 export const CONTRACT_ADDRESS = "Token contract address"; //string
